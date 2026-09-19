3ede91ed1725c39bb1d2f1095465f3a8bf67f143
Zhijun1933 <zhijun.he@yale.edu>
2026-09-19 19:00:12 -0400

    Delete crsv-atlas directory

--- crsv-atlas/src/build.js
+++
@@ -1,7 +0,0 @@
-const fs=require('fs');
-let s=fs.readFileSync('shell.html','utf8');
-s=s.replace('/*__CSS__*/',()=>fs.readFileSync('styles.css','utf8'))
-   .replace('/*__DATA__*/',()=>fs.readFileSync('../data/crsv-paragraphs-v2026.09.json','utf8'))
-   .replace('/*__JS__*/',()=>fs.readFileSync('app.js','utf8'));
-fs.writeFileSync('../index.html',s);
-console.log('built index.html', (s.length/1024).toFixed(0)+' KB');
